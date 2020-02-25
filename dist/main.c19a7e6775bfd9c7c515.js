f8e25cb044812fe96b8214b691fa2757699de28e
Bogdan Sklyarenko <bogdansklyarenko@Bogdans-MacBook-Pro.local>
2020-02-25 18:44:58 +0300

    Added file-loader for fonts and implemented auto generation of html pages

---
+++ dist/main.c19a7e6775bfd9c7c515.js
@@ -0,0 +1,35 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "Hglc":
+/*!************************!*\
+  !*** ./src/index.scss ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "tjUo":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var ramda__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ramda */ "sX7g");
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ "Hglc");
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_1__);
+var _ref;
+
+
+
+console.log((_ref = {}) === null || _ref === void 0 ? void 0 : _ref.a);
+
+/***/ })
+
+},[["tjUo","runtime","vendors"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvaW5kZXguc2Nzcz9hNWRlIiwid2VicGFjazovLy8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIiwiYSJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQUEsdUM7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQ0FBO0FBQ0E7QUFFQUEsT0FBTyxDQUFDQyxHQUFSLFNBQVksRUFBWix5Q0FBWSxLQUFJQyxDQUFoQixFIiwiZmlsZSI6Im1haW4uYzE5YTdlNjc3NWJmZDljN2M1MTUuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW4iLCJpbXBvcnQgcmFtZGEgZnJvbSBcInJhbWRhXCI7XG5pbXBvcnQgXCIuL2luZGV4LnNjc3NcIjtcblxuY29uc29sZS5sb2coe30/LmEpO1xuIl0sInNvdXJjZVJvb3QiOiIifQ==
